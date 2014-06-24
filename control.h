08f2fd9071cd81154dc1efc91b90e49840a1a690
ernie0218e <ernie0218e@gmail.com>
2014-06-24 18:15:12 +0800

    Create control.h

---
+++ control.h
@@ -0,0 +1,8 @@
+
+void control(int *);	//use an array to store the keyboard event
+						/*offset 0: way
+								 1: spacebar
+								 2: special func
+								 3: music control
+								 4: ESC
+						*/
