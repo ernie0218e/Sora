0997bd2d120fbc2809bbff4624055e7511c33e13
ernie0218e <ernie0218e@gmail.com>
2014-06-24 18:18:27 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <iostream>
+#include <graphics.h>
+#include <stdio.h>
+#include <time.h>
+#include "menu.h"
+using namespace std;
+
+int music_flag;
+
+int main(int argc, char *argv[])
+{
+	FILE *fp = fopen("option.txt","r");
+	if(fp != NULL)
+		fscanf(fp,"%d",&music_flag);
+	else
+		music_flag = 1;
+		
+	fclose(fp);
+	
+	initwindow(768,960, "圢騇眳秫");
+    
+    main_menu();
+    
+    
+    closegraph();
+    return 0;
+}
