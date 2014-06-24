1dd1d88ef98c8c72222f1ddc765bdd3a9d0ff80b
ernie0218e <ernie0218e@gmail.com>
2014-06-24 18:17:36 +0800

    Create graph.h

---
+++ graph.h
@@ -0,0 +1,6 @@
+
+int gifinfo(const char *, int *, int *);	//get the width & height of a GIF file
+
+int bmpinfo(const char *, int *, int *); //get the width & height of a BMP file
+
+void graphinfo(const char *, int *, int *);		//get the width & height of a BMP file or a GIF file
