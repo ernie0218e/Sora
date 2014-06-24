1ce1066dceb2ecc89cad3a07fdfdb93d972987d7
ernie0218e <ernie0218e@gmail.com>
2014-06-24 18:06:28 +0800

    Create ai.h

---
+++ ai.h
@@ -0,0 +1,17 @@
+#ifndef dstruct
+#define dstruct "dstruct.h"
+#include dstruct
+#endif
+
+
+void easy(enemy **, enemy **, void **, int, int, bullet **, bullet **, void **, int, int);
+
+void medium_ai(plane *, enemy **, enemy **, void **, int, int, bullet **, bullet **, void **, int, int);
+
+void medium(plane *, enemy **, enemy **, void **, int, int);
+
+void high_ai(plane *, bullet *, enemy **, enemy **, void **, int, int, bullet **, bullet **, void **, int, int);
+
+void trace(enemy **, enemy **, bullet **, bullet **, void **, int, int, void **, int, int, plane *);
+
+void trace_bullet(bullet *,plane *);
